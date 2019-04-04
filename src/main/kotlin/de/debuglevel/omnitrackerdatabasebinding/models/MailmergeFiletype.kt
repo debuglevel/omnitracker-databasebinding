8f40e99f50e5f20f4f25ba751536228056ccb0ee
Marc Kohaupt <marc.kohaupt@avm-d.de>
2019-04-04 12:17:04 +0200

    Specify Mailmerge types

--- src/main/kotlin/de/debuglevel/omnitrackerdatabasebinding/models/MailmergeFiletype.kt
+++ src/main/kotlin/de/debuglevel/omnitrackerdatabasebinding/models/MailmergeFiletype.kt
@@ -1,6 +1,10 @@
 package de.debuglevel.omnitrackerdatabasebinding.models
 
 enum class MailmergeFiletype(val id: Int) {
-    Type0(0),
+    Word(0),
+    PDF(1),
+    Type16(16),
+    Type31(31),
     Type53(53),
+    Type160(160),
 }

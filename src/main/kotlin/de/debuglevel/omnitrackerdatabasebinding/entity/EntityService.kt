da7033b0a7d6828c71acead4e4ee224bc07b14ed
Marc Kohaupt <debuglevel@gmail.com>
2020-06-30 00:01:32 +0200

    Cache entities

--- src/main/kotlin/de/debuglevel/omnitrackerdatabasebinding/entity/EntityService.kt
+++ src/main/kotlin/de/debuglevel/omnitrackerdatabasebinding/entity/EntityService.kt
@@ -14,6 +14,34 @@ abstract class EntityService<T : Entity>(
      */
     protected abstract val name: String
     protected abstract val query: String
+    private val cache: MutableMap<Int, T> = mutableMapOf<Int, T>()
+
+    private fun putCache(entity: T) {
+        logger.trace { "Putting id=${entity.id} into $name cache..." }
+        cache[entity.id] = entity
+        logger.trace { "Put id=${entity.id} into $name cache (size: ${cache.size}" }
+    }
+
+    private fun putCache(entities: Map<Int, T>) {
+        val beforeCacheSize = cache.size
+        logger.trace { "Putting ${entities.size} ${name}s into $name cache (size: $beforeCacheSize..." }
+        cache.putAll(entities)
+        val afterCacheSize = cache.size
+        val differenceSize = afterCacheSize - beforeCacheSize
+        logger.trace { "Put $differenceSize ${name}s $name cache (size: $afterCacheSize" }
+    }
+
+    private fun getFromCache(id: Int): T? {
+        logger.trace { "Getting id=${id} from $name cache..." }
+        val entity = cache[id]
+        if (entity != null) {
+            logger.trace { "Got id=${id} from $name cache" }
+        } else {
+            logger.trace { "Missed id=${id} in $name cache" }
+        }
+
+        return entity
+    }
 
     fun getAll(): Map<Int, T> {
         logger.debug { "Getting ${name}s..." }
@@ -32,12 +60,33 @@ abstract class EntityService<T : Entity>(
             entities
         }
 
+        // TODO: could be useful to clear cache as we just retrieved all entities
+        putCache(entities)
+
         logger.debug { "Got ${entities.size} ${name}s" }
         return entities
     }
 
     fun get(id: Int): T? {
         logger.debug { "Getting $name id=$id..." }
+
+        val cachedEntity = getFromCache(id)
+        val entity = if (cachedEntity != null) {
+            cachedEntity
+        } else {
+            val entity = getFromDatabase(id)
+            if (entity != null) {
+                putCache(entity)
+            }
+            entity
+        }
+
+        logger.debug { "Got ${name}: $entity" }
+        return entity
+    }
+
+    private fun getFromDatabase(id: Int): T? {
+        logger.debug { "Getting $name id=$id from database..." }
 
         val entity = databaseService.getConnection().use { connection ->
             val sqlStatement = connection.createStatement()
@@ -50,7 +99,7 @@ abstract class EntityService<T : Entity>(
             }
         }
 
-        logger.debug { "Got ${name}: $entity" }
+        logger.debug { "Got $name id=$id from database..." }
         return entity
     }
 
